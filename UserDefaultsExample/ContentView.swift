e9e743fbb70887b82cbd19cc8ecc36b2cf3af7bb
Stefan Bayne <stefanbayne@gmail.com>
2022-11-04 14:47:02 -0400

    Finished the demo tutorial.

--- UserDefaultsExample/ContentView.swift
+++ UserDefaultsExample/ContentView.swift
@@ -6,12 +6,43 @@
 //
 
 import SwiftUI
-import CoreData
 
 struct ContentView: View {
+    
+    @State var text: String = UserDefaults.standard.string(forKey: "TEXT_KEY_STRING") ?? ""
+    @State var input: String = ""
+    
+    
+    
     var body: some View {
-        Text("hellow world.")
-            .padding()
+        Form {
+            Section(header: Text("Enter text to count:")) {
+                TextField("Add any input you would like to Save: ", text: $input)
+            }
+            
+            Section(header: Text("Letter Count ")) {
+                let countLetters = input.filter{ $0 != " " }.count
+                
+                if(countLetters > 30) {
+                    Text(String(countLetters)).foregroundColor(Color.purple)
+                } else {
+                    input == "" ? Text("Waiting...") : Text(String(countLetters)).foregroundColor(.blue)
+                    
+                }
+            }
+            
+            Section(header: Text("Events")) {
+                Button("Save User Data") {
+                    UserDefaults.standard.set(input, forKey: "TEXT_KEY_STRING")
+                    text = input
+                    print("Saved value: \(input)")
+                }
+            }
+            Section(header: Text("Saved Data:")) {
+                Text(text).lineLimit(5)
+            }
+            
+        }
     }
 }
 
