457a31920e37bbd5b1c0c1d8bb7c65221aea9b48
Stefan Bayne <stefanbayne@gmail.com>
2022-11-15 10:12:53 -0500

    added a linear gradient for practice.

--- UserDefaultsExample/ContentView.swift
+++ UserDefaultsExample/ContentView.swift
@@ -12,36 +12,37 @@ struct ContentView: View {
     @State var text: String = UserDefaults.standard.string(forKey: "TEXT_KEY_STRING") ?? ""
     @State var input: String = ""
     
-    
+    let thisBackground = LinearGradient(colors: [.white,.gray], startPoint: .topLeading, endPoint: .bottomTrailing)
     
     var body: some View {
-        Form {
-            Section(header: Text("Enter text to count:")) {
-                TextField("Add any input you would like to Save: ", text: $input)
-            }
-            
-            Section(header: Text("Letter Count ")) {
-                let countLetters = input.filter{ $0 != " " }.count
+        ZStack {
+            Form {
+                Section(header: Text("Enter text to count:")) {
+                    TextField("Add any input you would like to Save: ", text: $input)
+                }
                 
-                if(countLetters > 30) {
-                    Text(String(countLetters)).foregroundColor(Color.purple)
-                } else {
-                    input == "" ? Text("Waiting...") : Text(String(countLetters)).foregroundColor(.blue)
+                Section(header: Text("Letter Count ")) {
+                    let countLetters = input.filter{ $0 != " " }.count
                     
+                    if(countLetters > 30) {
+                        Text(String(countLetters)).foregroundColor(Color.purple)
+                    } else {
+                        input == "" ? Text("Waiting...") : Text(String(countLetters)).foregroundColor(.blue)
+                        
+                    }
+                }
+                
+                Section(header: Text("Events")) {
+                    Button("Save User Data") {
+                        UserDefaults.standard.set(input, forKey: "TEXT_KEY_STRING")
+                        text = input
+                        print("Saved value: \(input)")
+                    }
+                }
+                Section(header: Text("Saved Data:")) {
+                    Text(text).lineLimit(5)
                 }
             }
-            
-            Section(header: Text("Events")) {
-                Button("Save User Data") {
-                    UserDefaults.standard.set(input, forKey: "TEXT_KEY_STRING")
-                    text = input
-                    print("Saved value: \(input)")
-                }
-            }
-            Section(header: Text("Saved Data:")) {
-                Text(text).lineLimit(5)
-            }
-            
         }
     }
 }
